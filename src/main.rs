0595af03a18bba402b147b8d9ce4b0320f3ab5d6
Sasha <whitecherry14@proton.me>
2024-01-09 11:51:34 +0100

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+  let x:&str = "Hello, world!"; // Immutable variable
+  print_type_of(&x);
+}
+
+fn print_type_of<T>(_: &T) {
+  // Get variable and use the standard library to print the type 
+  println!("{}", std::any::type_name::<T>());
+}
